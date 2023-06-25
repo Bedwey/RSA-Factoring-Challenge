1ee171d811d823fc5d3cc3912b28f23d4c532313
Bedwey <bedweyy@gmail.com>
2023-06-24 19:19:53 -0700

    test 🔨

--- factors.c
+++ factors.c
@@ -1,17 +1,25 @@
 #include "factors.h"
 
-void factorize(long int n)
+void factorize(mpz_t num)
 {
-	long int i;
-	for (i = 2; i < n; i++)
-	{
-		if (n % i == 0)
-		{
-			printf("%ld=%ld*%ld\n", n, n/i, i);
-			return;
-		}
-	}
-	printf("%ld is a prime number\n", n);
+    mpz_t i, factor, quotient, remainder, sqrt_num;
+    mpz_inits(i, factor, quotient, remainder, sqrt_num, NULL);
+    mpz_set_ui(i, 2);
+    mpz_sqrt(sqrt_num, num);
+
+    while (mpz_cmp(i, sqrt_num) <= 0)
+    {
+        mpz_fdiv_qr(quotient, remainder, num, i);
+        if (mpz_cmp_ui(remainder, 0) == 0)
+        {
+            gmp_printf("%Zd=%Zd*%Zd\n", num, quotient, i);
+            mpz_set(num, quotient);
+            break;
+        }
+        mpz_add_ui(i, i, 1);
+    }
+
+    mpz_clears(i, factor, quotient, remainder, sqrt_num, NULL);
 }
 
 void handle_timeout(int sig)
@@ -31,30 +39,33 @@ void setup_alarm()
 
 int main(int argc, char *argv[])
 {
-	FILE *fp;
-	long int n;
-	setup_alarm();
 	alarm(5);
+	setup_alarm();
+	FILE *fp;
+	char line[MAX_LINE_LENGTH];
+	mpz_t num;
+	mpz_init(num);
 
 	if (argc != 2)
 	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		fprintf(stderr, "Usage: factors <file> %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-        
+
 	fp = fopen(argv[1], "r");
 
-        if (!fp)
-        {
+	if (!fp)
+	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
-        }
-
-	while (fscanf(fp, "%ld", &n) == 1)
-	{
-		factorize(n);
 	}
-	
-	fclose(fp);
+
+	while (fgets(line, MAX_LINE_LENGTH, fp) != NULL)
+	{
+		mpz_set_str(num, line, 10);
+		factorize(num);
+	}
+
+	mpz_clear(num);
 	return 0;
 }
--- factors.h
+++ factors.h
@@ -4,11 +4,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <math.h>
 #include <unistd.h>
+#include <gmp.h>
+
+#define MAX_LINE_LENGTH 1000
 
 void setup_alarm();
 void handle_timeout(int sig);
 
-void factorize(long int n);
+void factorize(mpz_t num);
 
 #endif
